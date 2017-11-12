3f65b28731867c00d748abae37d5e383488da190
Class Account <ee106a-adj@imail.EECS.Berkeley.EDU>
2017-11-11 17:21:28 -0800

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa17/class/ee106a-adj/ros_workspaces/proj/src
+# Build directory: /home/cc/ee106a/fa17/class/ee106a-adj/ros_workspaces/proj/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(planning_baxter)
+SUBDIRS(lab4_cam)
