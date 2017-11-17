f05286e8fb971e23314b4054d4197414eb74f84e
Class Account <ee106a-adj@imail.EECS.Berkeley.EDU>
2017-11-17 14:39:42 -0800

    contours

--- src/planning_baxter/image_test.py
+++ src/planning_baxter/image_test.py
@@ -17,37 +17,42 @@ class image_converter:
   def callback(self,data):
     try:
       img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      img2 = img
     except CvBridgeError as e:
       print(e)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    ret,thresh = cv2.threshold(gray,25,255,1)
-
-    contours,h = cv2.findContours(thresh,1,2)
+    ret,thresh = cv2.threshold(gray,150,255,1)
 
     cv2.imshow('thresh', thresh)
 
-    for cnt in contours:
-        approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-        print len(approx)
-        if len(approx)==5:
-            print "pentagon"
-            cv2.drawContours(img,[cnt],0,255,-1)
-        elif len(approx)==3:
-            print "triangle"
-            cv2.drawContours(img,[cnt],0,(0,255,0),-1)
-        elif len(approx)==4:
-            print "square"
-            cv2.drawContours(img,[cnt],0,(0,0,255),-1)
-        elif len(approx) == 9:
-            print "half-circle"
-            cv2.drawContours(img,[cnt],0,(255,255,0),-1)
-        elif len(approx) > 15:
-            print "circle"
-            cv2.drawContours(img,[cnt],0,(0,255,255),-1)
+    dst = cv2.cornerHarris(thresh, 2, 3, 0.04)
+    #result is dilated for marking the corners, not important
+    dst = cv2.dilate(dst,None)
 
-    cv2.imshow('img',img)
+    # Threshold for an optimal value, it may vary depending on the image.
+    img[dst>0.25*dst.max()]=[0,0,255]
+
+    cv2.imshow('dst', img)
+    contours,h = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+
+    for cnt in contours:
+        approx = cv2.approxPolyDP(cnt,0.02*cv2.arcLength(cnt,True),True)
+        if len(approx)==4:
+            if cv2.contourArea(approx) > 1000 and cv2.contourArea(approx) < 10000:
+              print approx
+              cv2.drawContours(img2,[approx],0,(0,255,0),-1)
+              #cv2.drawContours(img2,[cnt],0,(0,0,255),-1)
+        """
+        elif len(approx) > 15:
+            if cv2.contourArea(approx) > 100 and cv2.contourArea(approx) < 10000:
+              cv2.drawContours(img2,[cnt],0,(0,0,255),-1)
+        """
+
+    cv2.imshow('img2', img2)
+    
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
